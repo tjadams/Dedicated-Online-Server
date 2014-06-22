c0ffa8b8fc3525538d72658a6f3eb33255c05730
Tyler Adams <notgiving@youmyemail.com>
2014-06-22 17:49:24 -0400

    Socket now recognizes connection with client. Removed old code and fixed undefined variable errors arising from sock

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var PORT = 8484;
 var server = net.createServer();
 server.listen(PORT, HOST);
 
+
+// handle first connection stuff (if this is called multiple times I'll need to add more logic)
 server.on('connection', function(sock) {
 
 
@@ -22,91 +24,9 @@ server.on('connection', function(sock) {
     });
 
     sock.on('close', function(data) {
-        console.log(sock.remoteAddress +':'+ sock.remotePort+' has disconnected');
+        console.log(this.remoteAddress +':'+ this.remotePort+' has disconnected with data: '+data);
     });
 
 
 }).listen(PORT, HOST);
-console.log('Server hosted on ' + server.address().address +':'+ server.address().port);
-
-//var express = require('express');
-//
-//var app = express();
-//app.set('port', 8484);
-//
-//app.listen(app.get('port'), function() {
-//    console.log("Server listening on port " + this.address().port);
-//});
-//
-// Socket.io server listens to our app
-//var io = require('socket.io').listen(app);
-//io.on('connection', function(socket){
-//    console.log("User: "+socket.toString()+" has connected");
-//});
-
-
-/*
- // Load the TCP Library
- net = require('net');
-
- var clients = [];
- var port = "8484"
-
- net.createServer(function (socket) {
- // identifies connected client
- socket.name = socket.remoteAddress + ":" + socket.remotePort;
- console.log(socket.name +" has connected to app.js");
- //
- //    // Put this new client in the list
- //    clients.push(socket);
- //
- //    // Send a nice welcome message and announce
- //    socket.write("Welcome " + socket.name + "\n");
- //    broadcast(socket.name + " joined the chat\n", socket);
-
- // Handle incoming messages from clients.
- socket.on('data', function (data) {
- console.log("'data' type of Data: "+data+" from "+socket.name);
- //  broadcast(socket.name + "> " + data, socket);
- // console.log("socket.name + "> " + data, socket");
- });
- // Handle incoming messages from clients.
- socket.on("data", function (data) {
- console.log("'data' type of Data: "+data+" from "+socket.name);
- //  broadcast(socket.name + "> " + data, socket);
- // console.log("socket.name + "> " + data, socket");
- });
-
- socket.on('', function (data) {
- console.log(" '' type of Data: "+data+" from "+socket.name);
- //  broadcast(socket.name + "> " + data, socket);
- // console.log("socket.name + "> " + data, socket");
- });
-
-
- // Remove the client from the list when it leaves
- socket.on('end', function () {
- console.log(socket.name +" has disconnected");
- //        clients.splice(clients.indexOf(socket), 1);
- //        broadcast(socket.name + " left the chat.\n");
- });
-
- // Send a message to all clients
- function broadcast(message, sender) {
- //        clients.forEach(function (client) {
- //            // Don't want to send it to sender
- //            if (client === sender) return;
- //            client.write(message);
- //        });
- //        // Log it to the server output too
- process.stdout.write(message)
- }
-
- }).listen(port);
-
- // Put a friendly message on the terminal of the server.
- console.log("Server running on port "+port+"\n");
-
- */
-
-
+console.log('Server hosted on ' + HOST +':'+ PORT);
