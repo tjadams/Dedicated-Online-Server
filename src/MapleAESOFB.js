43552bdd076d6bb106101607d077a43863bdc375
tjadams <robocustom@hotmail.com>
2014-06-22 21:00:19 -0400

    Initial creation of the MapleStory client's class and a class that handles part of the MapleStory encryption.

--- src/MapleAESOFB.js
+++ src/MapleAESOFB.js
@@ -1,3 +1,21 @@
-/**
- * Created by tyler.adams on 2014-06-22.
- */
+var crypto = require('crypto');
+
+function MapleAESOFB (key, iv, mapleVersion){
+    this.iv = iv;
+    // TODO investigate (short) cast
+    this.mapleVersion = (((mapleVersion >> 8) & 0xFF) | ((mapleVersion << 8) & 0xFF00));
+    // TODO investigate what type of AES algorithm i need
+    // it's aes 256 because the key has 32 bytes and as a result, 256 bits
+    this.cipher = crypto.createCipher('aes256',key);
+
+
+    // TODO investigate this method call
+    //  this.cipher = crypto.createCipheriv('aes256',key, iv);
+};
+
+MapleAESOFB.prototype.toString = function () {
+    return ("Cipher: " + this.cipher + " mapleVersion: " + this.mapleVersion+ " iv: " + this.iv);
+};
+
+// required for "importing this class" as in Java
+ module.exports = MapleAESOFB;
--- src/MapleClient.js
+++ src/MapleClient.js
@@ -1,3 +1,20 @@
-/**
- * Created by tyler.adams on 2014-06-22.
- */
+function MapleClient(sendCypher, recvCypher, sock) {
+    this.send = sendCypher;
+    this.receive = recvCypher;
+    this.session = sock;
+};
+
+MapleClient.prototype.toString = function () {
+    return ("" + this.session + " " + this.send + " " + this.receive);
+};
+
+MapleClient.prototype.setWorld = function (world) {
+    this.world = world;
+};
+
+MapleClient.prototype.setChannel = function (channel) {
+    this.channel = channel;
+};
+
+// required for "importing this class" as in Java
+module.exports = MapleClient;
