dc22250666f58e447752be1cf06efd91c59ab405
Tyler Adams <notgiving@youmyemail.com>
2014-06-26 23:15:47 -0400

    Started writing handshake packet to get to the login screen.

--- src/MaplePacketCreator.js
+++ src/MaplePacketCreator.js
@@ -1,3 +1,38 @@
 /**
  * Created by Tyler Adams on 26/06/2014.
  */
+
+function MaplePacketCreator()  {
+
+}
+
+MaplePacketCreator.prototype.getHello = function (MAPLEVERSION, ivSend, ivRecv) {
+    // create a buffer that will have a size of 8 bytes
+    var buffer = new Buffer("ucs2");
+    writeShort(0x0E, buffer);
+    writeShort(mapleVersion, buffer);
+    writeShort(1, buffer);
+    write(49, buffer);
+    write(recvIv, buffer);
+    write(sendIv, buffer);
+    write(8, buffer);
+
+
+    // TODO create packet from buffer by converting buffer to a byte array
+};
+
+MaplePacketCreator.prototype.writeShort = function(short, buffer){
+    write(short & 0xFF, buffer);
+    write((short >>> 8)& 0xFF, buffer);
+    this.buffer = buffer;
+}
+
+function write(byte, buffer){
+   // TODO use the nodejs library for writing to a buffer
+
+
+    this.buffer = buffer;
+}
+
+// required for "importing this class" as in Java
+module.exports = MaplePacketCreator;
