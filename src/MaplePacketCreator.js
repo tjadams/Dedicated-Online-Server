3a04faaaf5dd20c9a6331eddb55c676b1b66a7e2
Tyler Adams <robocustom@hotmail.com>
2014-06-27 00:13:20 -0400

    Exported the handshake method. Removed buffer to array conversion.

--- src/MaplePacketCreator.js
+++ src/MaplePacketCreator.js
@@ -4,35 +4,44 @@
 
 function MaplePacketCreator()  {
 
-}
 
-MaplePacketCreator.prototype.getHello = function (MAPLEVERSION, ivSend, ivRecv) {
+};
+
+var getHello = function (MAPLEVERSION, ivSend, ivRecv) {
     // create a buffer that will have a size of 8 bytes
     var buffer = new Buffer("ucs2");
     writeShort(0x0E, buffer);
-    writeShort(mapleVersion, buffer);
+    writeShort(MAPLEVERSION, buffer);
     writeShort(1, buffer);
     write(49, buffer);
-    write(recvIv, buffer);
-    write(sendIv, buffer);
+    write(ivRecv, buffer);
+    write(ivSend, buffer);
     write(8, buffer);
 
+    // create packet from buffer by converting buffer to a byte array
+/*    var array = [buffer.length];
+    for(var i=0; i<buffer.length; i++) {
+        array[i]=buffer[i];
+    }
 
-    // TODO create packet from buffer by converting buffer to a byte array
+    return array;
+    */
+    return buffer;
 };
 
-MaplePacketCreator.prototype.writeShort = function(short, buffer){
+function writeShort(short, buffer){
     write(short & 0xFF, buffer);
     write((short >>> 8)& 0xFF, buffer);
+
     this.buffer = buffer;
-}
+};
 
 function write(byte, buffer){
-   // TODO use the nodejs library for writing to a buffer
-
-
+    buffer.write(""+byte, "ucs2");
     this.buffer = buffer;
-}
+};
 
-// required for "importing this class" as in Java
-module.exports = MaplePacketCreator;
+// required for importing a "static method" as in Java
+module.exports = {
+    getHello: getHello
+};
