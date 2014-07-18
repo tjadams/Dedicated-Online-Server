4edd74f125e5ef1a08c0f687870dff2f5c6a21b9
Tyler Adams <robocustom@hotmail.com>
2014-07-17 23:57:23 -0400

    create skeleton for ServerlistRequestHandler

--- src/handlers/ServerlistRequestHandler.js
+++ src/handlers/ServerlistRequestHandler.js
@@ -1,3 +1,25 @@
 /**
- * Created by Tyler Adams on 29/06/2014.
+ * Created by Tyler Adams on 17/07/2014.
  */
+
+var MaplePacketCreator = require('../MaplePacketCreator.js');
+var MapleClient = require('../MapleClient.js');
+
+function ServerlistRequestHandler(){
+
+};
+
+ServerlistRequestHandler.prototype.handlePacket = function(packet, client){
+    console.log("ServerlistRequestHandler handlePacket");
+};
+
+// TODO EDIT ALL LOGIC
+ServerlistRequestHandler.prototype.validateState = function(client){
+    return true;
+};
+
+ServerlistRequestHandler.prototype.toString = function(){
+    return "ServerlistRequestHandler";
+};
+
+module.exports = ServerlistRequestHandler;
