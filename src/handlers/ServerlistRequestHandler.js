5c4c11ff7d5b54b7df34536dea2119fc927924dd
Tyler Adams <robocustom@hotmail.com>
2014-07-24 23:11:43 -0400

    finished ServerlistRequestHandler

--- src/handlers/ServerlistRequestHandler.js
+++ src/handlers/ServerlistRequestHandler.js
@@ -4,6 +4,8 @@
 
 var MaplePacketCreator = require('../MaplePacketCreator.js');
 var MapleClient = require('../MapleClient.js');
+var Server = require('../../app.js');
+var ServerConstants = require('../ServerConstants.js');
 
 function ServerlistRequestHandler(){
 
@@ -11,6 +13,19 @@ function ServerlistRequestHandler(){
 
 ServerlistRequestHandler.prototype.handlePacket = function(packet, client){
     console.log("ServerlistRequestHandler handlePacket");
+
+    var server = Server.getInstance();
+
+    var worlds = server.getWorlds();
+    var world;
+    for (var i = 0; i < worlds.length; i++) {
+        world = worlds[i];
+        c.announce(MaplePacketCreator.getServerList(world.id, ServerConstants.WORLD_NAMES[world.id], world.flag, world.eventmessage, world.channels));
+    }
+    c.announce(MaplePacketCreator.getEndOfServerList());
+    c.announce(MaplePacketCreator.selectWorld(0));
+    c.announce(MaplePacketCreator.sendRecommended(server.worldRecommendedList));
+
 };
 
 // TODO EDIT ALL LOGIC
