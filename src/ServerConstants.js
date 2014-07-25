9c7ccecf571766892999d943f02fd626d8a42bdb
Tyler Adams <robocustom@hotmail.com>
2014-07-24 23:15:21 -0400

    initialized server constants and worlds

--- src/ServerConstants.js
+++ src/ServerConstants.js
@@ -1,3 +1,4 @@
 /**
  * Created by Tyler Adams on 24/07/2014.
  */
+exports.WORLD_NAMES = ["Scania", "Bera", "Broa", "Windia"];
--- src/world.js
+++ src/world.js
@@ -1,3 +1,16 @@
 /**
- * Created by Tyler Adams on 24/07/2014.
+ * Created by Tyler Adams on 29/06/2014.
  */
+
+var World = module.exports = function World(stuff){
+   this.id = 0;
+   this.flag = 0;
+   this.eventmessage = "";
+   this.channels = null;
+};
+
+World.prototype.toString = function(){
+    return "AcceptToSHandler";
+};
+
+module.exports = World;
