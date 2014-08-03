fc1d2935e563e60722b588e0bb4387bec030ce22
Tyler Adams <robocustom@hotmail.com>
2014-08-02 23:29:11 -0400

    create a type of WZ file depending on the input path name

--- src/MapleDataProviderFactory.js
+++ src/MapleDataProviderFactory.js
@@ -1,3 +1,31 @@
 /**
  * Created by Tyler Adams on 02/08/2014.
  */
+var fs = require('fs');
+
+var MapleDataProviderFactory = function(){
+
+};
+
+MapleDataProviderFactory.getDataProvider = function(input){
+
+   return getWZ(input, false);
+};
+
+function getWZ(input, provideImages){
+
+    // todo check this
+    var endsWithWz = input.toLowerCase().splice(input.length-2, input.length);
+    var stats = fs.lstatSync(input);
+
+    if (endsWithWz.equals("wz") && !stats.isDirectory()) {
+        // todo inside WZFile, add error catching
+
+        return new WZFile(input, provideImages);
+    } else {
+        // todo inside XMLWZFile, add error catching
+        return new XMLWZFile(input);
+    }
+};
+
+module.exports = MapleDataProviderFactory;
